2b829102bb1d0cc476ec3e9c02481a6b8df90ee8
UnaiGMorchon <unaitxu79@gmail.com>
2023-05-09 23:41:48 +0200

    link de las routes y pagina de producto indivudual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,25 @@
-import data from "./data";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 /* import logo from "./logo.svg";
 import "./App.css";
  */
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <a href='/'> amaiie</a>
-      </header>
-
-      <main>
-        <h1>featured Products</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.nameproduct} />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.nameproduct}</p>
-                </a>
-                <p>
-                  <strong>{product.price}€</strong>
-                </p>
-
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/'> amaiie</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />}></Route>
+            <Route path='/' element={<HomeScreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
