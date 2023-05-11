6fe46995cf7c6c88f185db4b0bfc23327d6bf19a
victoria-pr <victoriaperojas@gmail.com>
2023-05-11 21:24:05 +0200

    solucionado problemas de iconos, no consigo ver el SigninScreen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,25 +4,45 @@ export const Store = createContext();
 
 const initialState = {
     cart: {
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
     },
 };
 function reducer(state,action) {
     switch (action.type) {
         case 'CART_ADD_ITEM':
             // add item to cart
+            const newItem = action.payload;
+            const existItem = state.cart.cartItems.find(
+                (item) => item._id === newItem._id
+            );
+            const cartItems = existItem
+                ? state.cart.cartItems.map((item) =>
+                    item._id === existItem._id ? newItem : item
+                )
+                : [...state.cart.cartItems, newItem];
+                localStorage.setItem('cartItems', JSON.stringify(cartItems));
+                return { ...state, cart: { ...state.cart, cartItems } };
             return {
                 ...state,
                 cart: { 
                     ...state.cart,
-                    cartItems: [...state.cart.cartItems, action.payload],
-                },
-            };
+                    cartItems}};
+        case 'CART_REMOVE_ITEM': {
+            // remove item from cart
+            const cartItems = state.cart.cartItems.filter(
+                (item) => item._id !== action.payload._id
+            );
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
+            return { ...state, cart: { ...state.cart, cartItems } };
+        }
+ 
 default:
     return state;
 }
 }
-
+ 
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
