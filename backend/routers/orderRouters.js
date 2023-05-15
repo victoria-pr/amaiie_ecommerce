e3b9ddc70acfe63789d19ed49ef163ab8ef14fda
UnaiGMorchon <unaitxu79@gmail.com>
2023-05-15 11:42:04 +0200

    integracion paypal OK

--- backend/routers/orderRouters.js
+++ backend/routers/orderRouters.js
@@ -2,6 +2,7 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
 import { isAuth } from "../utils.js";
+
 const orderRouter = express.Router();
 orderRouter.post(
   "/",
@@ -33,6 +34,35 @@ orderRouter.get(
     }
   })
 );
+
+// Ruta para marcar una orden como pagada
+orderRouter.put(
+  "/:id/pay",
+
+  //isAuth,
+
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+
+      res.send({ message: "Orden pagada.", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Orden no encontrada." });
+    }
+  })
+);
+
 export default orderRouter;
 
 /* 
@@ -44,9 +74,6 @@ import Order from "../models/orderModel.js";
 //import { isAuth } from "../utils.js";
 
 const orderRouter = express.Router();
-
-
-
 
  */
 
